c7b3bb00e7bba47bc5ba8cad548f0057ee96feb1
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-08-10 17:02:04 +0200

    -Added Player.cs -Added 2DCharacterMovement.cs Thanks to Brackeys 2D Tutorial -Learned To work with Unity 2D Tilemap system. -Added SimpleCamera movement script -Added Player Statemachine with custom animations

---
+++ PlatformerJourney/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public int Lives { get { return lives; } set { lives = value; } }
+
+	private int maxLives = 5;
+	private int lives;
+	
+	private void Start()
+	{
+		lives = maxLives;
+	}
+
+	private void Update()
+	{
+		UpdateLives();
+	}
+
+	private void UpdateLives()
+	{
+		if (lives == 0)
+		{
+			//Destroy(this.gameObject);
+		}
+
+		if(lives >= maxLives)
+		{
+			lives = maxLives;
+		}
+	}
+}
---
+++ PlatformerJourney/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private CharacterController2D controller;
+	[SerializeField] private float runSpeed = 40f;
+	private float horizontalMove = 0f;
+	private bool jump = false;
+
+	private Animator anim;
+
+	private void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update()
+    {
+		horizontalMove	= Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if(horizontalMove > 0)
+		{
+			anim.SetBool("isWalking", true);
+		}
+		else if(horizontalMove < 0)
+		{
+			FlipPlayer(true);
+			anim.SetBool("isWalking", true);
+		}
+		else
+		{
+			anim.SetBool("isWalking", false);
+		}
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+
+	private void FlipPlayer(bool isFlipped)
+	{
+		if (isFlipped)
+		{
+			transform.localScale = new Vector3(-1, 1, 1);  //face left
+		}
+		else
+		{
+			transform.localScale = new Vector3(1, 1, 1);  //face right
+		}
+	}
+}
