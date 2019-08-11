dda00b83eaf3aecbeeaa3b8638a94caf30fd7f8e
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-08-11 04:11:11 +0200

    -Added FinishLine -Added DoorSwitch -Added Small Animations

---
+++ PlatformerJourney/Assets/Scripts/ButtonDoorSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDoorSwitch : MonoBehaviour
+{
+
+	[SerializeField] private Sprite switchSprite, doorSprite;
+	[SerializeField] private SpriteRenderer sRenSwitch, sRenDoor;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == Tags.Player)
+		{
+			sRenSwitch.sprite = switchSprite;
+			sRenDoor.sprite = doorSprite;
+		}
+	}
+}
